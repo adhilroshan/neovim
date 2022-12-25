2d96703ffb24102fa677e2fbac35235d8ed9bad4
Adhil Roshan <58517896+adhilroshan@users.noreply.github.com>
2022-12-25 22:12:20 +0530

    initial commit

---
+++ lua/core/plugin_config/lsp_config.lua
@@ -0,0 +1,18 @@
+require("mason").setup()
+require("mason_lspconfig").setup({
+  ensure_installed = { "sumneko_lua" }
+})
+
+local on_attach = function(_, _)
+  vim.keymaps.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+  vim.keymaps.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+  vim.keymaps.set('n', 'gd', vim.lsp.buf.definition, {})
+  vim.keymaps.set('n', 'gi', vim.lsp.buf.implementation, {})
+  vim.keymaps.set('n', 'gr', require('telescope.builtin').lsp.references, {})
+  vim.keymaps.set('n', 'K', vim.lsp.buf.hover, {})
+end
+
+require("lspconfig").sumneko_lua.setup {
+  on_attach=on_attach
+}
